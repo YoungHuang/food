ae1817795a87a164f303c8d3f1e9895ae1222a8f
yonghuang <h00138136@163.com>
2013-10-29 13:01:20 +0800

    update

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,9 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world! HY");
+	response.success("Hello world! HY");
+});
+
+AV.Cloud.define("wx", function(request, response) {
+	response.success(request.echostr);
 });
